721e53eca51b0806501f031f5d3a0463c0dd8057
TyRyan1 <tyryan@trentu.ca>
2024-11-28 23:09:06 -0500

    Added TestBorrowReturn method and fixed LibraryManagemmentTest

--- LibraryManagementTest.java
+++ LibraryManagementTest.java
@@ -4,15 +4,13 @@ import org.junit.Before;
 
 public class LibraryManagementTest {
 
-	
-	
 	@Test
 	public void testBookID(){
 		try {
 			Book a = new Book(100, "Math");
 			Book b = new Book(999, "Sci");
 			Book c = new Book(1000, "Bio");
-			fail("could not add book");
+			
 			
 			//check getId Method
 			assertEquals(100, a.getId());
@@ -22,11 +20,43 @@ public class LibraryManagementTest {
 			//check getTitle Method
 			assertEquals("Math", a.getTitle());
 			assertEquals("Sci", b.getTitle());
-			assertEquals("Sci", c.getTitle());
+			assertEquals("Eng", c.getTitle());
 		}
 		catch(Exception e){
-			System.out.println("Error: " + e.getMessage());
+			fail("could not add book");
 		}
 
 	}
+	@Test
+	public void testBorrowReturn(){
+		try {
+			Book newBook = new Book(200, "X");
+			Member newMember = new Member(3000,"Ty");
+			
+
+			assertTrue("Book is unavailable",newBook.isAvailable());//Check if book is available
+			
+			assertTrue(Transaction.getTransaction().borrowBook(newBook,newMember));//check if user can borrow a book	
+
+			assertFalse("Book is available",newBook.isAvailable()); //Check if book is unavailable
+			
+			assertFalse(Transaction.getTransaction().borrowBook(newBook,newMember));//check if user can borrow a book twice
+		
+			assertTrue(Transaction.getTransaction().returnBook(newBook,newMember));//check if book can be returned
+	
+			assertTrue("Transaction not completed",newBook.isAvailable());//check if book is available
+			
+			assertFalse(Transaction.getTransaction().returnBook(newBook,newMember));//check if book can be returned twice
+			
+			
+		
+		} catch (Exception e) {
+			fail("ERROR");
+			
+		}
+		
+		
+	}
+	
+	
 }
